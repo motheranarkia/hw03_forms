96f173a9edc454bb9bd2001ab4578ce3780cfbcd
motheranarkia <96246181+motheranarkia@users.noreply.github.com>
2022-02-04 17:24:23 +0300

    Ревью

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -32,8 +32,9 @@ class Post(models.Model):
         on_delete=models.SET_NULL,
         related_name='posts'
     )
+
     class Meta:
         ordering = ['-pub_date']
-        
+
     def __str__(self):
         return self.text[:15]
