d65143fcad713ccd84a98e5e43412ead43dd4755
motheranarkia <96246181+motheranarkia@users.noreply.github.com>
2022-02-10 19:32:26 +0300

    sprint 5 in process

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -38,7 +38,7 @@ class TaskPagesTests(TestCase):
             reverse('posts:post_edit', kwargs={'post_id': self.post.pk}): 'posts/create_post.html',
             reverse('posts:create_post'): 'posts/create_post.html'
         }
-        for template, reverse_name in templates_pages_names.items():
+        for reverse_name, template in templates_pages_names.items():
             with self.subTest(reverse_name=reverse_name):
                 response = self.authorized_client.get(reverse_name)
                 self.assertTemplateUsed(response, template) 
